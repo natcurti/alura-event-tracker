a9059bc668e2a267b69b63f275d0c17cccfe32a4
natcurti <naticurti01@gmail.com>
2024-03-06 16:12:51 -0300

    Implement filter using recoil

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -2,18 +2,31 @@ import React from "react";
 import Evento from "../Evento";
 import Filtro from "../Filtro";
 import style from "./ListaDeEventos.module.scss";
+import useListaDeEventos from "../../state/hooks/useListaDeEventos";
 import { useRecoilValue } from "recoil";
-import { listaDeEventosState } from "../../state/atom";
+import { IFiltroDeEventos } from "../../interfaces/IFiltroDeEventos";
+import { filtroDeEventos } from "../../state/atom";
 
-const ListaDeEventos: React.FC<{
-  aoFiltroAplicado: (data: Date | null) => void;
-}> = ({ aoFiltroAplicado }) => {
-  const eventos = useRecoilValue(listaDeEventosState);
+const ListaDeEventos: React.FC = () => {
+  const eventos = useListaDeEventos();
+
+  const filtro = useRecoilValue<IFiltroDeEventos>(filtroDeEventos);
+
+  const eventosFiltrados = eventos.filter((evt) => {
+    if (!filtro.data) {
+      return true;
+    }
+    const mesmoDia =
+      filtro.data.toISOString().slice(0, 10) ===
+      evt.inicio.toISOString().slice(0, 10);
+    return mesmoDia;
+  });
+
   return (
     <section>
-      <Filtro aoFiltroAplicado={aoFiltroAplicado} />
+      <Filtro />
       <div className={style.Scroll}>
-        {eventos.map((evento) => (
+        {eventosFiltrados.map((evento) => (
           <Evento evento={evento} key={evento.id} />
         ))}
       </div>
