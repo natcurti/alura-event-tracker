9602ff046c934681ac4b45a21e8b16a89294ebed
natcurti <naticurti01@gmail.com>
2024-03-06 13:34:15 -0300

    Refactor form component to handle state using recoil

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function App() {
       <div className={style.App}>
         <div className={style.Coluna}>
           <Card>
-            <Formulario aoSalvar={adicionarEvento} />
+            <Formulario />
           </Card>
           <hr />
           <Card>
